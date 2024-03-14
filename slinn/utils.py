29e2864870592cbc13c3d6e6eeb6ef9a87e3266a
Mark <markradin6886@gmail.com>
2024-03-14 22:32:59 +0300

    140324a
    ignoring ssl.SSLError exceptions, rewrited Server.reload(), update readme

--- slinn/utils.py
+++ slinn/utils.py
@@ -1,4 +1,16 @@
-import re
+import re, threading
+
+
+class StoppableThread(threading.Thread):
+    def __init__(self,  *args, **kwargs):
+        super(StoppableThread, self).__init__(*args, **kwargs)
+        self._stop_event = threading.Event()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
 
 
 def restartswith(text, reg):
